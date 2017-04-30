ac092d01722db8da3ae2f58cab1c81504692f2dc
ahmedtaaw <ahmedtaaw@gmail.com>
2017-04-30 03:35:59 +0200

    console.log reactive

--- myreactiveform/src/app/app.component.ts
+++ myreactiveform/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{FormControl, FormGroup} from '@angular/forms';
+
+import {Contacts} from './contacts.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  contactinfo: FormGroup;
+
+  ngOnInit(){
+    this.contactinfo = new FormGroup({
+      name: new FormControl(''),
+      contactno: new FormControl('')
+    });
+  }
+
+  onSubmit({value,valid}:{value:Contacts,valid:boolean}){
+    console.log(value,valid);
+  }
 }
