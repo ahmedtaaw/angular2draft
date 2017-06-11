fa0b031048624e30eb65ac705f70af0a84022715
ahmedtaaw <ahmedtaaw@gmail.com>
2017-06-12 00:30:27 +0200

    json

--- loadshow/src/app/app.module.ts
+++ loadshow/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PostIdComponent } from './post-id/post-id.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostIdComponent
   ],
   imports: [
     BrowserModule,
