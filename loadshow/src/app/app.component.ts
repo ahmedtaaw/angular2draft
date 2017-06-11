b40b4c017e4f4687d3f545e75326a840838a65d5
ahmedtaaw <ahmedtaaw@gmail.com>
2017-06-12 00:52:58 +0200

    post service

--- loadshow/src/app/app.component.ts
+++ loadshow/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import {postService}from './post-id/post-id.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[postService]
 })
 export class AppComponent {
   title = 'app works!';
--- loadshow/src/app/post-id/post-id.component.ts
+++ loadshow/src/app/post-id/post-id.component.ts
@@ -1,44 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {postService} from './post-id.service';
+import {Iposts} from './post-id';
 @Component({
   selector: 'app-post-id',
   templateUrl: './post-id.component.html',
   styleUrls: ['./post-id.component.css']
 })
 export class PostIdComponent implements OnInit {
- 
-  constructor() { }
+  posts:Iposts[];
+  constructor(private _postService:postService) { }
 
   ngOnInit() {
+    this.posts=this._postService.getposts();
   }
- postComments:any[]=[
-  {
-    "postId": 1,
-    "id": 1,
-    "name": "id labore ex et quam laborum",
-    "email": "Eliseo@gardner.biz",
-    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
-  },
-  {
-    "postId": 1,
-    "id": 2,
-    "name": "quo vero reiciendis velit similique earum",
-    "email": "Jayne_Kuhic@sydney.com",
-    "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
-  },
-  {
-    "postId": 1,
-    "id": 3,
-    "name": "odio adipisci rerum aut animi",
-    "email": "Nikita@garfield.biz",
-    "body": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
-  },
-  {
-    "postId": 1,
-    "id": 4,
-    "name": "alias odio sit",
-    "email": "Lew@alysha.tv",
-    "body": "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati"
-  }
-  ]
+
 }
